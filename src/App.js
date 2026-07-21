8f5550b4394a440d9fbe7dc0e5dae8cee945d5d6
TinyDotPink <jurgeit1978@gmail.com>
2026-07-21 08:33:33 +0200

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>React Weather App</h1>
-      <footer>
-        This project was coded by Christine and is
-        <a
-          href="https://github.com/TinyDotPink/react-w5"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open-sourced.
-        </a>
-      </footer>
+    <div className="App">
+      <div className="container">
+        <h1>React Weather App</h1>
+        <footer>
+          This project was coded by Christine and is
+          <a
+            href="https://github.com/TinyDotPink/react-w5"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
